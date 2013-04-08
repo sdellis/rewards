aa5753035e62ac1fee626bc97b820a7a7330cbc4
shaune <shaune@shaune-VirtualBox.(none)>
2013-04-08 13:12:45 +0100

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rewards::Application.config.secret_token = 'f4b30e091e32db6b40f321c47f47e18908bc4f5b5330a2bff2cbb51dc3fdba80da6627b491d6da9db361bce58631216da521e69df89418c93431d340b33fe11d'
